e74b5bb2d833fd1e77322ecc11f7868f6a670d68
jeffrey ram pineda <jeffreyrampineda@gmail.com>
2021-09-01 21:45:15 -0400

    fixed: _blank security warning

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ function Footer() {
             <div className="text-center">
                 <small>
                     <p>This is a demo</p>
-                    <a href="https://github.com/jeffreyrampineda/wikka" target="_blank">Source</a>
+                    <a href="https://github.com/jeffreyrampineda/wikka" target="_blank" rel="noreferrer">Source</a>
                     &nbsp;|&nbsp;
-                    <a href="https://jeffreyram.pineda.org" target="_blank">Jeffrey Ram Pineda</a>
+                    <a href="https://jeffreyram.pineda.org" target="_blank" rel="noreferrer">Jeffrey Ram Pineda</a>
                     <p>Wikka © 2021 All Rights Reserved.</p>
                 </small>
             </div>
