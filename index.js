9f842516ef1467e97e652c79986a532b158c62cc
jeffrey ram pineda <jeffreyrampineda@gmail.com>
2022-04-15 17:59:00 -0400

    added debug utility

--- index.js
+++ index.js
@@ -3,19 +3,39 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const app = require("./app");
+const debug = require('debug')('wikka:server');
 const http = require("http");
 
-const port = process.env.PORT || 3001;
+// Get port from environment and store in Express.
+const port = normalizePort(process.env.PORT || '3001');
 app.set("port", port);
 
 // Create HTTP server.
 const server = http.createServer(app);
 
-server.listen(port, () =>
-  console.log(`The server is running at http://localhost:${port}/`)
-);
-server.on("error", onError);
+// Listen on provided port, on all network interfaces.
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 
+// Normalize a port into a number, string, or false.
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+// Event listener for HTTP server "error" event.
 function onError(error) {
   if (error.syscall !== "listen") {
     throw error;
@@ -36,4 +56,11 @@ function onError(error) {
     default:
       throw error;
   }
+}
+
+// Event listener for HTTP server "listening" event.
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("Listening on " + bind);
 }
