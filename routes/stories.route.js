1b1aef6121144a01192f9e7f2c274145137bae3f
jeffrey ram pineda <jeffreyrampineda@gmail.com>
2025-04-17 20:18:35 -0400

    deploy: client only

--- routes/stories.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const stories_controller = require("../controllers/stories.controller");
-
-router.get("/api/stories", stories_controller.stories_list);
-
-router.get("/api/stories/:id", stories_controller.stories_detail);
-
-module.exports = router;
